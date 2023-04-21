341c0e5d05cd0a08258c7427dc1726b4bfe34d72
JIeunhuh <entks301@naver.com>
2023-04-21 11:35:22 +0900

    component 2

---
+++ src/02/MyClockImage.js
@@ -0,0 +1,10 @@
+import logo from '../logo.svg';
+
+const MyClockImage=()=>{
+    return(
+         
+        <img src={logo} className='App-logo' alt='logo'/>
+        
+    );
+}
+export default MyClockImage;
