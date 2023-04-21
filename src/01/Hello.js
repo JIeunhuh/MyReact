a00236783587ae222d221623e787d9a23af1eceb
JIeunhuh <entks301@naver.com>
2023-04-21 11:05:36 +0900

    component1

---
+++ src/01/Hello.js
@@ -0,0 +1,21 @@
+import logo from '../logo.svg';
+import './Hello.css';
+//react component의 기본 구조
+const Hello = ()=>{ //사용자 정의태그는 반드시 return() method가 있어야 함
+  let name = '허지은';
+  let git = 'https://github.com/JIeunhuh';
+    return(
+      <main className='container'>
+      <article data-theme='dark'>
+        <img src={logo} className='App-logo' alt='logo'/>
+        <footer>
+          <hgroup>
+            <h1>{name}</h1>
+            <h2><a href={git}>{git}</a></h2>
+            </hgroup>
+        </footer>
+      </article>
+      </main>
+    ); //부모 node인 div는 무조건 하나만 있어야 함!
+}
+export default Hello; //똑같은 이름으로 내보내야 함
