ba2ffc93c63b4488cbb6d814200844d6177adf02
JIeunhuh <entks301@naver.com>
2023-04-27 16:31:54 +0900

    app(js)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+//import Hello from './01/Hello'; //파일에 있는 태그를 실행할 거임 
+//import MyClock from './02/MyClock';
+ //import Box from './03/Box';
+//import method from 'path';
+//import MyDiv from './04/MyDiv';
+import Frcst from './05/Frcst';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   // <Hello/>
+   //<MyClock/>
+   //<Box />
+   //<MyDiv/>
+   <Frcst/>
   );
 }
 
+//class로 쓰면 안됨, className으로 반드시 써야함!!
+//import되어 있는 img 가져올때 src={} alt='' 써야함
+//react는 반드시 종료태그가 있어야 함, 태그가 끝나기 전에 '/' 붙여줌
+//<> </> : 
 export default App;
