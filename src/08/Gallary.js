8b7f00d33facc275c456a31d0aa8b5fe04f22042
JIeunhuh <entks301@naver.com>
2023-05-11 13:08:03 +0900

    Gallary_project

--- src/08/Gallary.js
+++ src/08/Gallary.js
@@ -1,7 +1,9 @@
 import style from './Gallery.module.css';
-import { useEffect, useRef } from 'react';
+import GallaryList from './GallaryList';
+import { useEffect, useRef, useState } from 'react';
 /*한국관광공사 관광사진갤러리 키워드 검색, 목록조회*/
 const Gallary = () => {
+    const [gList, setgList] = useState();
     const txt = useRef();
     //컴포넌트가 맨처음 랜더링 되면
     useEffect(() => {
@@ -9,16 +11,27 @@ const Gallary = () => {
         txt.current.focus();
     }, [])
 
+
     //등록버튼 onclick
     const showImg = (e) => {
         e.preventDefault();
-        const kw =encodeURI(txt.current.value); //encodeURI()
-        console.log(txt.current.value+' '+kw);
+        const kw = encodeURI(txt.current.value); //encodeURI()
+        console.log(txt.current.value + ' ' + kw);
+        let url = 'https://apis.data.go.kr/B551011/PhotoGalleryService1/gallerySearchList1?serviceKey=2cTsIbMSNEakGj3HNSZtwQLbXXeuyM8AuaNHrr3dKA3L5Xi5w%2F0U6Rs3CwSIjl0DbsffGzIToXtTrtThSIlJCw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&keyword=';
+        url = url + kw + '&_type=json';
+        //fetch()
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((data) => setgList(data.response.body.items.item))
+            .catch((err) => console.log(err));
+        
+        console.log('url =' + url);
     };
 
     //취소버튼 onclick 
     const resetItem = () => {
-
+        txt.current.value = '';
+        txt.current.focus();
     };
     return (
         <main className="container">
@@ -32,6 +45,7 @@ const Gallary = () => {
                     </div>
                 </div>
             </article>
+            {gList && <GallaryList gList={gList} setgList={setgList} />}
         </main>
     );
 }
---
+++ src/08/GallaryList.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import style from './Gallery.module.css';
+const GallaryList = ({ gList }) => {
+
+    //const [lList, setlList] = useState();산
+    // const[rList,setrList]=useState();
+    let tags = [];
+    
+
+    //json data 배열로 받아서 키값 출력하기 
+    for (let row of gList) {
+
+        let temp = row.galSearchKeyword.split(','); //keword의 쉼표 삭제
+        console.log('row = ' + temp);
+
+        tags.push(
+            
+            <article className="grid">
+                <head className="grid"><h1>{row.galTitle}</h1><h4>{row.galPhotographyLocation}</h4></head>
+                <body><img src={row.galWebImageUrl}></img></body>
+                <footer><div className={style.kwtag}>{temp}</div></footer>
+            </article>
+
+        );
+             
+    }
+    // let tags2 =[];
+    // for (let i = 0; i < tags.length; i += 2) {
+      
+        
+    // }
+
+    return (
+
+        <article>
+            {tags}
+        </article>
+
+    );
+}
+export default GallaryList;
